652aae17c158614ae8750930e165bb27b69e9638
Mohit Parmanand -X (mparmana - HCL TECHNOLOGIES LIMITED at Cisco) <pythonmks@gmail.com>
2025-02-03 06:56:17 +0530

    updated UI and fixed multiple api call

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,59 +1,74 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { fetchBusRoutes } from '../api/fatchData';
 import RouteCard from '../components/RouteCard';
 
 export default function SearchPage() {
   const router = useRouter();
   const [routes, setRoutes] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  const fetchData = async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const sourceId = urlParams.get('source');
-    const destinationId = urlParams.get('destination');
-
-    if (!sourceId || !destinationId) {
-      router.push('/'); // Redirect to the home page if source or destination is not provided in the URL router.
-      return;
-    }
-    try {
-      setLoading(true);
-      const data = await fetchBusRoutes(sourceId, destinationId);
-      setRoutes(data);
-    } catch (err) {
-      setError('Failed to fetch bus routes');
-    } finally {
-      setLoading(false);
-    }
-  };
+  const fetchedRef = useRef(false);
 
   useEffect(() => {
-    console.log('useEffect called');
-    fetchData();
+    const fetchData = async () => {
+      if (fetchedRef.current) return; // Exit if we've already fetched
 
+      const urlParams = new URLSearchParams(window.location.search);
+      const sourceId = urlParams.get('source');
+      const destinationId = urlParams.get('destination');
+
+      if (!sourceId || !destinationId) {
+        router.push('/');
+        return;
+      }
+
+      try {
+        setLoading(true);
+        fetchedRef.current = true; // Mark as fetched before the API call
+        const data = await fetchBusRoutes(sourceId, destinationId);
+        setRoutes(data);
+      } catch (err) {
+        setError('Failed to fetch bus routes');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   if (loading) {
-    return <div><p className="text-[#201d27] text-4xl font-semibold tracking-tight">Loading routes...</p></div>;
+    return <div><p className="text-[#201d27] text-4xl font-semibold tracking-tight text-center">Loading routes...</p></div>;
   }
 
   if (error) {
-    // if(error === 'Failed to fetch bus routes') {
-    //   return <div className="text-[#201d27] text-4xl font-semibold tracking-tight">
-    //     <p >No Internet!!</p>
-    //     <p>Check your Connection.</p>
-    //     </div>
-    // }
-    return <div className="text-[#201d27] text-4xl font-semibold tracking-tight">
-      <p className='text-center mt-10'>You got us!!</p>
-      <p className='p-4 text-centre'>No data available for this route, we keep on adding thousands of routes.</p>
-      <p className='p-4 text-center'>See you later ♡</p>
+    if (error === "Failed to fetch bus routes") {
+      return <div className="bg-gray-100 rounded-3xl shadow-xl text-[#201d27] text-4xl font-semibold tracking-tight">
+        <p >No Internet!!</p>
+        <p>Check your Connection.</p>
+      </div>
+    }
+    return <div className='bg-gray-100 rounded-3xl shadow-xl text-[#201d27] text-4xl font-semibold tracking-tight'>
+      <p>Try Again Later!!!</p>
+      <p className='p-4 text-center text-red-500 text-md'>Error: {error}</p>
+    </div>;
+  }
+
+  if (routes.message === "Routes Not found") {
+
+    return <div className="flex justify-center items-center flex-col bg-gray-100 rounded-3xl shadow-xl p-4 text-[#201d27] text-4xl font-semibold tracking-tight">
+      <p className='text-center mt-8'>You got us!!</p>
+      <p className='pt-4'>No data available for this route, we keep on adding thousands of new routes.</p>
+      <p className='p-4 text-center'>See you later ♡.</p>
+      <div className=''>
+        <button className='mt-4 p-4 font-light rounded-full bg-[#201d27] text-white text-lg tracking-normal' onClick={() => router.push('/')}>Search Different Route</button>
+      </div>
     </div>
   }
+
 
   return (
     <div className='bg-gray-100 rounded-3xl shadow-sm'>
